f042e30a04a3eb835fa9d5ba188aec35496c8299
Nick Hsiao <hsiao.nicholas@gmail.com>
2022-03-23 13:14:34 -0700

    simple go web server

--- main.go
+++ main.go
@@ -1,12 +1,45 @@
 package main
 
-import( 
+import (
 	"fmt"
 	"log"
 	"net/http"
 )
 
-func main(){
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "POST request successful")
+	firstname := r.FormValue("firstname")
+	lastname := r.FormValue("lastname")
+	email := r.FormValue("email")
+	fmt.Fprintf(w, "First Name = %s\n", firstname)
+	fmt.Fprintf(w, "Last Name = %s\n", lastname)
+	fmt.Fprintf(w, "email = %s\n", email)
+}
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 Not Found", http.StatusNotFound)
+		return
+	}
+	//only accept get method otherwise
+	if r.Method != "GET" {
+		http.Error(w, "method is not supported", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w, "Hello!")
+}
+func main() {
+	fileServer := http.FileServer(http.Dir("./"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
 
-	
+	fmt.Print("Starting server at port 8000\n")
+	if err := http.ListenAndServe(":8000", nil); err != nil {
+		log.Fatal(err)
+	}
+
 }
